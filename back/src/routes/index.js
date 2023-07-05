9aab00071bbdf9e54297b62ae42063d3ed87074d
Matias Sequeira <matiasseq_11oct@hotmail.com>
2023-07-05 16:41:31 -0300

    appointments router

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const usersRouter = require('./usersRouter')
-
+const appointmentsRouter = require("./appointments") 
 const router = express.Router();
 
 
 //users
 router.use('/users', usersRouter)
+
+//appointments 
+router.use('/appointments', appointmentsRouter)
 
 
 
