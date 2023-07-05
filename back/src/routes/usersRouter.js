828fa948c15ac3708e9e7e360abc019928afdeac
iHoerth <hoerth.ignacio@gmail.com>
2023-07-05 16:53:42 -0300

    usersRouter bug

--- back/src/routes/usersRouter.js
+++ back/src/routes/usersRouter.js
@@ -4,7 +4,7 @@ const usersRouter = express.Router();
 const getUserById = require('../handlers/users/getUsersById');
 const getAllUsers = require('../handlers/users/getAllUsers');
 
-usersRouter.get('/', getUserById);
-usersRouter.get('/:id', getAllUsers);
+usersRouter.get('/:id', getUserById);
+usersRouter.get('/', getAllUsers);
 
 module.exports = usersRouter;
