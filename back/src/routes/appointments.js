d3f5fb37f66ead029e94176018a21485ae0aafca
Matias Sequeira <matiasseq_11oct@hotmail.com>
2023-07-05 17:12:38 -0300

    MATT11 y su handlers

--- back/src/routes/appointments.js
+++ back/src/routes/appointments.js
@@ -1,27 +1,11 @@
 const express = require("express")
-const axios = require("axios")
 const appointmentsRouter = express.Router()
 
-appointmentsRouter.get('/:userId', async (req, res)=>{
-    const { userId } = req.params;
-    try {
-        const results = await axios.get(`https://jsonplaceholder.typicode.com/posts/${userId}`)
-        const user = results.data
-        res.status(200).json(user)
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
+const getAllUsersAppointments = require('../handlers/appointments/getAllUsersAppointments')
+const getAllUsersIdAppointments = require ('../handlers/appointments/getUserIdAppointments')
 
-appointmentsRouter.get('/', async (req, res)=>{
-    try {
-        const results = await axios.get(`https://jsonplaceholder.typicode.com/posts`)
-        const posts = results.data
-        res.status(200).json(posts)
-    } catch (error) {
-        res.status(400).json(error)
-    }
-})
+appointmentsRouter.get('/:userId', getAllUsersAppointments)
+appointmentsRouter.get('/', getAllUsersIdAppointments)
 
 
 
