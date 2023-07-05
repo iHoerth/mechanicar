cd69e5df2e9e1865e6f25d95bddcdcb086110fcb
iHoerth <hoerth.ignacio@gmail.com>
2023-07-05 17:58:49 -0300

    sequelize connected

--- back/src/db.js
+++ back/src/db.js
@@ -4,18 +4,24 @@ const { DB_NAME, DB_PORT, DB_PASSWORD, DB_USER, DB_HOST } = process.env;
 
 const CarModel = require('../src/models/Car');
 const UserModel = require('../src/models/User');
-const AppointmentModel = require('../src/models/Appointment');
+// const AppointmentModel = require('../src/models/Appointment');
 
 const sequelize = new Sequelize(
-  `postgress://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   { logging: false }
 );
 
 CarModel(sequelize);
 UserModel(sequelize);
-AppointmentModel(sequelize);
+// AppointmentModel(sequelize);
 
-const { Car, User, Appointment } = sequelize.models;
+const { Car, User } = sequelize.models;
+
+Car.belongsTo(User);
+User.hasMany(Car);
+
+// User.belongsToMany(Appointment, { through: 'appointments_users' });
+// Appointment.belongsToMany(User, { through: 'appointments_users' });
 
 sequelize
   .authenticate()
