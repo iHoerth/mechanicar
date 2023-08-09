0b3529d621d2d1e91b2f503ca86b0c39b86e3b4b
iHoerth <hoerth.ignacio@gmail.com>
2023-08-09 18:05:36 -0300

    fetchAppointment & fake appointment

--- back/src/controllers/appointments/fetchAppointmentsByEmail.js
+++
@@ -1,9 +0,0 @@
-const { Appointments, User } = require('../../db');
-
-const fetchAppointmentsByEmail = async (email) => {
-  const user = await User.findOne({ where: { email: email } });
-  const appointments = await Appointments.findAll({ where: { UserId: user.id } });
-  return appointments;
-};
-
-module.exports = { fetchAppointmentsByEmail };
--- back/src/controllers/fakeData/index.js
+++ back/src/controllers/fakeData/index.js
@@ -1,17 +1,38 @@
 const { faker } = require('@faker-js/faker');
-const { User, Car } = require('../../db');
+const { User, Car, Appointment } = require('../../db');
 
-const generateFakeData = async (/* limit = 5 */) => {
+const generateFakeData = async (limit = 10) => {
   const createdUsers = [];
-  // limit > 50 && (limit = 50);
-  const userOne = await User.create({
+  const createdAppointments = [];
+  const createdCars = [];
+
+  const userZero = await User.create({
     name: 'John Doe',
     email: 'johndoe@gmail.com',
     phone: 12345678,
   });
-  createdUsers.push(userOne);
+  createdUsers.push(userZero);
 
-  for (let i = 0; i < 10; i++) {
+  const carZero = await Car.create({
+    brand: 'Ford',
+    model: 'Fiesta',
+    year: 2023,
+    plate: 'AAA',
+    userId: userZero.id,
+  });
+  createdCars.push(carZero);
+
+  const appointmentZero = await Appointment.create({
+    issueDate: '8/8/2023',
+    deadline: '8/9/2023',
+    price: faker.commerce.price({ symbol: '$', min: 3000, max: 10000 }),
+    description: faker.lorem.sentence(3),
+    CarId: carZero.id,
+  });
+  createdAppointments.push(appointmentZero);
+
+  // Users
+  for (let i = 0; i < limit; i++) {
     const user = await User.create({
       name: faker.internet.userName(),
       email: faker.internet.email(),
@@ -19,30 +40,32 @@ const generateFakeData = async (/* limit = 5 */) => {
     });
     createdUsers.push(user);
   }
-  // Appointments
 
   // Cars
-  const generateFakeCars = async () => {
-    await Car.create({
-      brand: 'Ford',
-      model: 'Fiesta',
-      year: 2023,
-      plate: 'AAA',
-      userId: 1,
+  for (let i = 0; i < limit; i++) {
+    const randomUserId = createdUsers[Math.floor(Math.random() * createdUsers.length)].id;
+    const car = await Car.create({
+      brand: faker.vehicle.manufacturer(),
+      model: faker.vehicle.vehicle(),
+      year: 1990 + Math.floor(Math.random() * 32),
+      plate: faker.vehicle.vrm(),
+      UserId: randomUserId,
     });
+    createdCars.push(car);
+  }
 
-    for (let i = 0; i < 10; i++) {
-      const randomUserId = createdUsers[Math.floor(Math.random() * createdUsers.length)].id;
-      Car.create({
-        brand: faker.vehicle.manufacturer(),
-        model: faker.vehicle.vehicle(),
-        year: 1990 + Math.floor(Math.random() * 32),
-        plate: faker.vehicle.vrm(),
-        UserId: randomUserId,
-      });
-    }
-  };
-  generateFakeCars();
+  // Appointments
+  for (let i = 0; i < limit; i++) {
+    const randomCarId = createdCars[Math.floor(Math.random() * createdCars.length)].id;
+    const appointment = await Appointment.create({
+      issueDate: faker.date.recent().toString(),
+      deadline: faker.date.soon().toString(),
+      price: faker.commerce.price({ symbol: '$', min: 3000, max: 10000 }),
+      description: faker.lorem.sentence(3),
+      CarId: randomCarId,
+    });
+    createdAppointments.push(appointment);
+  }
 };
 
 module.exports = { generateFakeData };
